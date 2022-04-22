84806fbc8667d7b216773b7cd5fc7c6152e2dda2
kapreign <info@kapreign.com>
2022-04-22 23:33:35 +0530

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `yash-project`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
+
+export default config;
